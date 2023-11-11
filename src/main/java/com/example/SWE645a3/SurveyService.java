5375b0417905f432ed9e08ec61f7e235932d16d8
MAMIDI LOKESH VENKATA SAI <36584697+lokesh-venkata-sai@users.noreply.github.com>
2023-11-11 16:35:13 -0500

    Added transactional tag

--- src/main/java/com/example/SWE645a3/SurveyService.java
+++ src/main/java/com/example/SWE645a3/SurveyService.java
@@ -7,8 +7,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import jakarta.transaction.Transactional;
+
 
 @Service
+@Transactional
 public class SurveyService {
     @Autowired
     SurveyRepository surveyRepository;
